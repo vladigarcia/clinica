d4ef151f1900a4bad6d5c8f0d8991a255db3152f
MAFRAlexan <144178838+vladigarcia@users.noreply.github.com>
2023-10-10 13:37:11 -0600

    creacion de Seeders genero

---
+++ database/seeders/generoSeeders.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class generoSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
