98a7ad7433fe2901273dfbb2f2fc9aca968c5a0e
JairoVespon <108412797+JairoVespon@users.noreply.github.com>
2023-10-10 14:40:22 -0600

    npse

--- resources/views/layout/footer.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout/template')
-
-@section('footer')
-    <footer>
-        Esta página se editó por última vez el 03 Oct 2023 a las 15:56.<br/>
-		El contenido está disponible bajo la licencia de Jairo y Vladimir
-    </footer>
-@endsection
--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -12,7 +12,68 @@
 
     <body>
         <header>
-            @yield('navbar')
+            <nav class="navbar navbar-expand-md navbar-light bg-light">
+                <div class="container">
+                    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+        
+                            <!-- Inicio -->
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{url('inicio')}}" aria-current="page"> Inicio <span class="visually-hidden">(current)</span></a>
+                            </li>
+        
+                            <!-- Pacientes -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pacientes</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownId">
+                                    <a class="dropdown-item" href="{{url('pacientes/index')}}">Ver</a>
+                                    <a class="dropdown-item" href="{{url('pacientes/create')}}">Agregar</a>
+                                </div>
+                            </li>
+        
+                            <!-- Medicos -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="{{url('')}}" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pacientes</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownId">
+                                    <a class="dropdown-item" href="#">Ver</a>
+                                    <a class="dropdown-item" href="#">Agregar</a>
+                                </div>
+                            </li>
+        
+                            <!-- Horarios -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="{{url('')}}" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pacientes</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownId">
+                                    <a class="dropdown-item" href="#">Ver</a>
+                                    <a class="dropdown-item" href="#">Agregar</a>
+                                </div>
+                            </li>
+        
+                            <!-- Clinicas -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="{{url('')}}" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pacientes</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownId">
+                                    <a class="dropdown-item" href="#">Ver</a>
+                                    <a class="dropdown-item" href="#">Agregar</a>
+                                </div>
+                            </li>
+        
+                            <!-- Citas -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="{{url('')}}" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pacientes</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownId">
+                                    <a class="dropdown-item" href="#">Ver</a>
+                                    <a class="dropdown-item" href="#">Agregar</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
 
         <main>
@@ -20,7 +81,8 @@
         </main>
 
         <footer>
-            @yield('footer')
+            Esta página se editó por última vez el 03 Oct 2023 a las 15:56.<br/>
+            El contenido está disponible bajo la licencia de Jairo y Vladimir
         </footer>
 
         <!-- Bootstrap JavaScript Libraries -->
