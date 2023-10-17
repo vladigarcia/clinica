6fc02b95f717bc7b9da41b22761fc08c77c32b37
JairoVespon <108412797+JairoVespon@users.noreply.github.com>
2023-10-17 14:01:46 -0600

    xuxa

--- database/migrations/2023_10_10_3_create_clinicas_table.php
+++ database/migrations/2023_10_10_3_create_clinicas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('clinicas', function (Blueprint $table) {
             $table->id('cod_clinica', 10);
             $table->string('nombre', 100);
-            $table->int('telefono', 8);
+            $table->string('telefono', 8);
             $table->string('direccion', 100);
             $table->timestamps();
         });
