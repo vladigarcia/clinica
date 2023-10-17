49b3d35a90d05a43ba20f3631c0b881b9fc088e0
JairoVespon <108412797+JairoVespon@users.noreply.github.com>
2023-10-17 11:22:14 -0600

    ppppp

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Paciente extends Model
 {
     use HasFactory;
+
+    public function genero()
+    {
+        return $this->belongsTo(Genero::class, 'genero_id', 'id');
+    }
 }
