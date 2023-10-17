7cdef60f0cbc429ef56b7ec9389dc0afd7c36af0
vladigarcia <144178838+vladigarcia@users.noreply.github.com>
2023-10-17 12:05:54 -0600

    tamales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,23 @@
-@extends('layouts/template')
+@extends('layouts.app')
 
-@section('title', 'Página Principal | Clínica José Remedos')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-@section('contenido')
-<main>
-    <div class="container py-4">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h1 class="text-center" style="color: #3498db;">Bienvenido a la Clinica José Remedos</h1>
-                        <hr>
-                        <h2 class="text-center" style="color: #555;">A cuál de nuestras plataformas desea ingresar?</h2>
-                        <hr>
-                        <h3 style="color: #333;">Formulario Pacientes</h3>
-                        <a href="{{ url('/pacientes') }}" class="btn btn-primary btn-sm">Ingresar<a>
-                    </div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
     </div>
-</main>
-<style>
-    .card {
-        border: 1px solid #3498db;
-        border-radius: 10px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-        margin: 10px;
-        background-color: #f9f9f9;
-        padding: 20px;
-    }
-
-    .card-body {
-        text-align: center;
-    }
-
-    .btn-primary {
-        background-color: #3498db;
-        border-color: #3498db;
-        color: #fff;
-    }
-</style>
-
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::resource( '/citas', CitasController::class );
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
