d2bab5b3330c5b916d5326d45549d610f1620c42
JairoVespon <108412797+JairoVespon@users.noreply.github.com>
2023-11-14 15:24:33 -0600

    mas cosa

--- database/migrations/2023_10_10_5_create_horarios_table.php
+++ database/migrations/2023_10_10_5_create_horarios_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('horarios', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('medico_id');
-            $table->string('dia_trabajo');
+            $table->unsignedBigInteger('dia_trabajo');
             $table->time('hora_e');
             $table->time('hora_s');
             $table->timestamps();
